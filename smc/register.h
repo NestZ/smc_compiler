712bb51fbd63775b3bbcc7540ac7d7f1c041eed6
Duxz <semada15@gmail.com>
2020-11-15 18:50:09 +0700

    jame ver.

--- smc/register.h
+++ smc/register.h
@@ -3,8 +3,10 @@ class Register{
 		//TODO add dynamic size
 		Register(){}
 		void reset(){}
-		void set(){}
-		void get(){}
+		void set(int register_no, int value){}
+		int get(int register_no){
+			return register_lst.at(register_no);
+		}
 	private:
 		const int MAX_REGISTER_SIZE = 8;
 		vector<int> register_lst = vector<int>(MAX_REGISTER_SIZE, 0);
--- smc/simulator/r_type.h
+++ smc/simulator/r_type.h
@@ -2,6 +2,14 @@
 
 class R_Type{
   public:
-    int add(){}
-    int nand(){}
+    void add(int32_t rs, int32_t rt, int32_t rd , Register reg){
+      int value;
+      value = reg.get(rs) + reg.get(rt);
+      reg.set(rd,reg.get(rs) + reg.get(rt));
+    } 
+
+    int nand(int32_t rs, int32_t rt, int32_t rd , Register reg){
+      int value;
+      value = (reg.get(rs)) & (reg.get(rt));
+    }
 };

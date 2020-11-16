1a3aa93ea2268061d87f73c76f2224daea3adaa6
winnieinyourarea <thepiggywin2000@gmail.com>
2020-11-16 12:16:10 +0700

    itype

--- smc/memory.h
+++ smc/memory.h
@@ -3,8 +3,10 @@ class Memory{
 		//TODO add dynamic size
 		Memory(){}
 		void reset(){}
-		void set(){}
-		void get(){}
+		void set(int memory_no, int value){}
+		int get(int memory_no){
+			return memory_lst[memory_no];
+		}
 	private:
 		const int MAX_MEMORY_SIZE = (1 << 16);
 		vector<int> memory_lst = vector<int>(MAX_MEMORY_SIZE, 0);
